375c5c474a0d9c955ec9c2608ad76d5ccdd50f0c
Spiros Eliopoulos <seliopou@gmail.com>
2013-02-24 23:32:23 -0500

    Working parser, with the exception of dates

--- src/Text/TOML/Parser.hs
+++ src/Text/TOML/Parser.hs
@@ -9,28 +9,51 @@ import Data.Attoparsec.Types
 
 import Text.TOML.Value
 
-document = comment
+document = smb *> many ekk <* endOfInput
+  where 
+    smb = skipMany blank
+    ekk = (eitherP keygroup keyval) <* smb
 
-value = array <|> bool <|> str <|> num -- <|> date
+keygroup = do
+    skipMany blank
+    between lbrace rbrace skey
+  where skey = keyg `sepBy` period
+
+keyval = do
+    k <- keyv
+    v <- equal *> value
+    return (k, v)
+
+keyg = lexeme $ takeWhile1 $ notInClass " \n]."
+keyv = lexeme $ takeWhile1 $ notInClass " \n="
+
+value = (array <?> "array")
+    <|> (bool  <?> "bool")
+    <|> (str   <?> "string")
+    <|> (num   <?> "number")-- <|> date
   where
     array = VArray <$> between lbrace rbrace (value `sepBy` comma)
-    bool  = VBool <$> (true *> return True <|> false *> return False)
+    bool = VBool <$> (true *> return True <|> false *> return False)
     str = VString <$> between quote quote (many (notChar '"'))
-    num = lexeme $ do
-        n <- number
+    num = do
+        n <- lexeme $ number
         case n of
             I n -> return $ VInteger n
             D d -> return $ VDouble d
 
 whatever p = p >> return ()
-lexeme p = do { many (many1 spc); p }
-spc   = whatever $ char ' ' <|> char '\t'
+lexeme p = do { x <- p; many spc; return x }
+spc   = char ' ' <|> char '\t'
 comment = whatever $ char '#' *> takeTill (=='\n')
+line p = p *> (lexeme endOfLine)
+blank = line $ lexeme $ (try comment) <|> return ()
 
 quote = lexeme $ string "\""
 lbrace = lexeme $ string "["
 rbrace = lexeme $ string "]"
 comma = lexeme $ string ","
+period = lexeme $ string "."
+equal = lexeme $ string "="
 true = lexeme $ string "true"
 false = lexeme $ string "false"
 
