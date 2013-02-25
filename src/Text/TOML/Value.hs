b9d9b01190e7e8f35833e794076bf9e45eb5fb1a
Spiros Eliopoulos <seliopou@gmail.com>
2013-02-24 19:28:26 -0500

    add value definition
    newtype delcaration is necessary because of circular reference error if
    it were just a type synonym.

---
+++ src/Text/TOML/Value.hs
@@ -0,0 +1,18 @@
+module Text.TOML.Value where
+
+import Data.Map ( Map )
+
+
+newtype TOML = TOML (Map String (Either TOML TOMLV))
+    deriving ( Eq, Ord )
+
+data TOMLV
+    = VString String
+    | VInteger Integer
+    | VDouble Double
+    | VBool Bool
+    | VArray [TOMLV]
+    | VDocument TOML
+    | VDate -- TODO
+    deriving ( Eq, Ord )
+
