fb4f71ceb630803ebd0430d80a0346b07a4e7e5e
Hampus Ramström <hampusram93@gmail.com>
2017-03-08 21:39:06 +0100

    Implemeneted ppTable, ppFloat, ppBoolean and ppArray

--- src/Text/Toml/Pretty.hs
+++ src/Text/Toml/Pretty.hs
@@ -13,23 +13,43 @@ module Text.Toml.Pretty (
 import Text.Toml.Types
 import Text.PrettyPrint.HughesPJ
 
+import           Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as M
+import           Data.Vector         as V
 import           Data.Text           (Text)
 import qualified Data.Text           as T
 
 ppNode :: Node -> Doc
 ppNode n = case n of
-    (VTable v)    -> undefined
+    (VTable v)    -> ppTable v
     (VTArray v)   -> undefined
     (VString v)   -> ppTomlString v
     (VInteger v)  -> undefined
-    (VFloat v)    -> undefined
-    (VBoolean v)  -> undefined
+    (VFloat v)    -> ppFloat v
+    (VBoolean v)  -> ppBoolean v
     (VDatetime v) -> undefined
-    (VArray v)    -> undefined
+    (VArray v)    -> ppArray v
 
 ppTomlString :: T.Text -> Doc
-ppTomlString str = doubleQuotes $ hcat $ map pp_char (T.unpack str)
+ppTomlString str = doubleQuotes $ hcat $ Prelude.map pp_char (T.unpack str)
     where pp_char '\\' = text "\\\\"
           pp_char '\"' = text "\\\""
           pp_char c    = char c
 
+ppFloat :: Double -> Doc
+ppFloat f = double f
+
+ppBoolean :: Bool -> Doc
+ppBoolean True = text "true"
+ppBoolean False = text "false"
+
+-- Unclear with fsep, vcat
+ppArray :: Vector Node -> Doc
+ppArray va = brackets $ fsep $ punctuate comma $ Prelude.map ppNode (V.toList va)
+
+ppTable :: Table -> Doc
+ppTable t = vcat $ tableToList $ M.toList t
+
+tableToList :: [(Text,Node)] -> [Doc]
+tableToList t = Prelude.map fsep (Prelude.map f t)
+    where f (x,y) = punctuate equals [text $ T.unpack x,ppNode y]
